3eb550187536afe3ed8e19b0690c5dc0f9c6d1a4
Crowdbotics <team@crowdbotics.com>
2021-08-05 19:45:18 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,6 @@
+export const globalOptions = {
+  name: "silent_voice_29531Identifier",
+  url: "https://silent_voice_29531Identifier.botics.co",
+  api: "https://silent_voice_29531Identifier.botics.co/api/v1"
+}
+export const modulesOptions = {}
